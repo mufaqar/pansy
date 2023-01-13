b2397ed2ee58d18e0bc31ac21682ebfaaa289133
Mufaqar Islam <mufaqar@gmail.com>
2023-01-13 19:39:22 +0500

    Commingsoon

--- pages/comming-soon.js
+++ pages/comming-soon.js
@@ -3,32 +3,31 @@ import React from 'react';
 import Footer from './components/footer';
 
 export default function Commingsoon() {
-    return (
-        <>
-
-  
-      
-        <section class="under-construction">
-            <header>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="menu-wrapper">
-                                <div class="menu-logo logo hi-res">
-                                    <a href="/"><img src="images/hi-res-logo.png" alt=""></a>
-                                </div>
-                                <div class="slide-icons">
-                                    <a href="#" class="ca ca-phone-two" target="_blank"></a>
-                                    <a href="#" class="ca ca-email" target="_blank"></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section class="under-construction">
+        <header>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="menu-wrapper">
+                  <div class="menu-logo logo hi-res">
+                    <a href="/">
+                      <img src="images/hi-res-logo.png" alt="" />
+                    </a>
+                  </div>
+                  <div class="slide-icons">
+                    <a href="#" class="ca ca-phone-two" target="_blank"></a>
+                    <a href="#" class="ca ca-email" target="_blank"></a>
+                  </div>
                 </div>
-            </header>
-        </section>
-     
-        <Footer />
+              </div>
+            </div>
+          </div>
+        </header>
+      </section>
+
+      <Footer />
     </>
   );
 }
