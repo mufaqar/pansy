d76d621cb1bd831dd4f2fa08bd50b32249f68a20
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-07-18 19:07:46 +0500

    updated

--- pages/components/counter-section.js
+++ pages/components/counter-section.js
@@ -3,7 +3,7 @@ import Count from './count'
 
 const Counter_Section = () => {
     return (
-        <section className='py-20 items-center bg-[url("/images/backgrounds/4.jpg")] bg-center bg-no-repeat bg-cover'>
+        <section className='py-20 items-center bg-blend-multiply bg-black/75 bg-[url("/images/backgrounds/4.jpg")] bg-center bg-no-repeat bg-cover'>
             <div className='conatiner mx-auto px-4 grid md:grid-cols-4 grid-cols-2 gap-7'>
                 {counts?.map(count => <Count key={count?.id} data={count} />)}
             </div>
