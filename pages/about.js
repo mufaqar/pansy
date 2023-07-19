74bd3dcdf319ff983a3bbfd520913b93a54a4138
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-07-19 15:53:25 +0500

    About

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import PageBanner from './components/page-banner';
 import TabsRender from './components/tabs';
+import Footer from './components/footer';
 
 export default function About() {
   return (
@@ -12,6 +13,7 @@ export default function About() {
         content="Toi is a full-sevice web design and mobile agency based in San Fransisco, Los Angeles, Austin and Beunos Aires. We take pride in what we do, believe in our work, and are motivated by the success of our clients."
         Custom_bg="bg-[url(/images/backgrounds/3.jpg)]" />
         <TabsRender />
+        <Footer />
     </main>
   );
 }
