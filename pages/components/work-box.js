553c69c3dafcdb617dccfdc02c6227eca45d5d2d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-16 15:10:26 +0500

    Portfolio

---
+++ pages/components/work-box.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function WorkBox({img, title, date}) {
+    return (
+        <div className='h-full relative group'>
+            <Image src={img} alt='img' width={100} height={100} className='w-full h-full' />
+            <div className='bg-txt_clr/60 group-hover:bg-[#6fdec6]/80 flex flex-col justify-between absolute top-4 bottom-4 left-4 right-4 py-12 px-4'>
+                <div className=''>
+                    <Link href="#" className='text-xs font-normal text-white block w-full mb-5'>
+                       {date}
+                    </Link>
+                    <Link href="#" className='text-lg font-semibold uppercase text-white block w-full'>
+                    {title}
+                    </Link>
+                </div>
+                <div>
+                    <Link href="#" className="text-base font-medium px-5 py-2.5 border-white border bg-white text-txt_clr hover:bg-txt_clr hover:text-white hover:border-txt_clr">
+                        Read more
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageBanner from "./components/page-banner";
+import WorkBox from './components/work-box';
 
 export default function Portfolio() {
   return (
@@ -7,8 +8,45 @@ export default function Portfolio() {
       <PageBanner
         title="Portfolio"
         content="Here are the key services to enhance your business performance"
-        Custom_bg="bg-[url(/images/backgrounds/2.jpg)]" />
+        Custom_bg="bg-[url(/images/backgrounds/2.jpg)]"
+      />
 
+      <section className='py-16'>
+        <div className='container mx-auto px-4 grid md:grid-cols-4 grid-cols-1'>
+          <WorkBox img="/images/blog/4-column/1.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/2.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/3.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/4.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/5.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/6.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/7.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+          <WorkBox img="/images/blog/4-column/8.jpg"
+            title="DESIGN YOU NEED EVERYDAY"
+            date=" 31 OCTOBER 2015"
+          />
+        </div>
+      </section>
     </main>
   )
 }
