e02c6bc43ab16d4393af08c407065bd5b4aa934a
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-16 15:56:54 +0500

    blog updated

--- pages/components/blog-tabs.js
+++ pages/components/blog-tabs.js
@@ -40,33 +40,36 @@ const Tabs = () => {
                     </ul>
                     <div className="relative flex flex-col min-w-0 mt-12">
                         <div className="py-5 flex-auto">
-                            <div className="tab-content grid md:grid-cols-3 grid-cols-1 gap-7">
-                                {Tabs_Data.map((item, idx) => {
-                                    return <div key={idx}>
-                                        <div>
-                                            <Image src={item?.img} height={250} width={250} alt="img" className='w-full object-cover ' />
-                                        </div>
-                                        <div className='py-7'>
-                                            <div className="mb-5">
-                                                <Link href="#" className="text-sm font-semibold uppercase px-4 py-1 border-primary border-y-2 bg-transparent text-primary hover:bg-primary hover:text-white">
-                                                    {item?.category}
-                                                </Link>
+                            {Tabs_Data.map((item, idx) => {
+                                return <div key={idx} className={`tab-content grid md:grid-cols-3 grid-cols-1 gap-7 ${openTab === item.id ? "block" : "hidden"}`}>
+                                    {item?.post?.map((post, _idx) => {
+                                        return <div key={_idx}>
+                                            <div>
+                                                <Image src={post?.img} height={250} width={250} alt="img" className='w-full object-cover ' />
                                             </div>
-                                            <Link href="#" className='text-lg font-semibold uppercase text-title_clr text-center mb-5'>
-                                                {item?.title}
-                                            </Link>
-                                            <p className='text-base font-medium text-txt_clr/80 mb-5'>
-                                                {item?.content}
-                                            </p>
-                                            <div className="mb-5">
-                                                <Link href="#" className="text-sm font-semibold uppercase py-1 text-primary  hover:text-primary/70">
-                                                    Read More
+                                            <div className='py-7'>
+                                                <div className="mb-5">
+                                                    <Link href="#" className="text-sm font-semibold uppercase px-4 py-1 border-primary border-y-2 bg-transparent text-primary hover:bg-primary hover:text-white">
+                                                        {item?.category}
+                                                    </Link>
+                                                </div>
+                                                <Link href="#" className='text-lg font-semibold uppercase text-title_clr text-center mb-5'>
+                                                    {post?.title}
                                                 </Link>
+                                                <p className='text-base font-medium text-txt_clr/80 mb-5'>
+                                                    {post?.content}
+                                                </p>
+                                                <div className="mb-5">
+                                                    <Link href="#" className="text-sm font-semibold uppercase py-1 text-primary  hover:text-primary/70">
+                                                        Read More
+                                                    </Link>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                })}
-                            </div>
+                                    })}
+                                </div>
+                            })}
+
                         </div>
                         <div className="flex justify-center mt-5">
                             <Link href="#" className="text-base font-semibold uppercase px-7 py-3 border-primary border-y-2 bg-transparent hover:bg-primary hover:text-white">
@@ -103,49 +106,145 @@ export const Tabs_Data = [
     {
         id: 1,
         category: "Show All",
-        title: "Design you need everyday",
-        img: "/images/blog/2-column/1.jpg",
-        link: "/simply-sync",
-        content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/1.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/2.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/3.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/4.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/5.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/6.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
     {
         id: 2,
         category: "Culture",
-        title: "Having fun with instagram",
-        img: "/images/blog/2-column/2.jpg",
-        link: "/simply-simp-agent",
-        content: "Believe it or not, I'm walking on ai. I never thought I could feel so free. Flying away on a,"
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/1.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/2.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
     {
         id: 3,
         category: "Life Style",
-        title: "New Approach to mobile trends new ui",
-        img: "/images/blog/2-column/3.jpg",
-        link: "/simply-transport-agent",
-        content: "Believe it or not, I'm walking on ai. I never thought I could feel so free. Flying away on a,"
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/3.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/4.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
     {
         id: 4,
         category: "Arts",
-        title: "About Process",
-        img: "/images/blog/2-column/4.jpg",
-        link: "/simply-transport-agent",
-        content: "Believe it or not, I'm walking on ai. I never thought I could feel so free. Flying away on a,"
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/5.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/6.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
     {
         id: 5,
         category: "Travel",
-        title: "About Process",
-        img: "/images/blog/2-column/5.jpg",
-        link: "/simply-transport-agent",
-        content: "Believe it or not, I'm walking on ai. I never thought I could feel so free. Flying away on a,"
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/1.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/2.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
     {
         id: 6,
         category: "Business",
-        title: "About Process",
-        img: "/images/blog/2-column/6.jpg",
-        link: "/simply-transport-agent",
-        content: "Believe it or not, I'm walking on ai. I never thought I could feel so free. Flying away on a,"
+        cat_link: "#",
+        post: [
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/3.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/4.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+            {
+                title: "Design you need everyday",
+                img: "/images/blog/2-column/5.jpg",
+                link: "#",
+                content: "Believe it or not, I'm walking on air. I never thought I could feel so free. Flying away on a,",
+            },
+        ],
     },
 ]
