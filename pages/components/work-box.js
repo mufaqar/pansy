3865f55d2abff2ca826306d04f71dc6632fda611
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-16 15:15:17 +0500

    position

--- pages/components/work-box.js
+++ pages/components/work-box.js
@@ -2,17 +2,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-export default function WorkBox({img, title, date}) {
+export default function WorkBox({ img, title, date }) {
     return (
         <div className='h-full relative group'>
             <Image src={img} alt='img' width={100} height={100} className='w-full h-full' />
-            <div className='bg-txt_clr/60 group-hover:bg-[#6fdec6]/80 flex flex-col justify-between absolute top-4 bottom-4 left-4 right-4 py-12 px-4'>
+            <div className='bg-txt_clr/60 group-hover:bg-[#6fdec6]/80 flex flex-col justify-between absolute top-3 bottom-3 left-3 right-3 py-12 px-4'>
                 <div className=''>
                     <Link href="#" className='text-xs font-normal text-white block w-full mb-5'>
-                       {date}
+                        {date}
                     </Link>
                     <Link href="#" className='text-lg font-semibold uppercase text-white block w-full'>
-                    {title}
+                        {title}
                     </Link>
                 </div>
                 <div>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -13,35 +13,43 @@ export default function Portfolio() {
 
       <section className='py-16'>
         <div className='container mx-auto px-4 grid md:grid-cols-4 grid-cols-1'>
-          <WorkBox img="/images/blog/4-column/1.jpg"
+          <WorkBox
+            img="/images/blog/4-column/1.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/2.jpg"
+          <WorkBox
+            img="/images/blog/4-column/2.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/3.jpg"
+          <WorkBox
+            img="/images/blog/4-column/3.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/4.jpg"
+          <WorkBox
+            img="/images/blog/4-column/4.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/5.jpg"
+          <WorkBox
+            img="/images/blog/4-column/5.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/6.jpg"
+          <WorkBox
+            img="/images/blog/4-column/6.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/7.jpg"
+          <WorkBox
+            img="/images/blog/4-column/7.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
-          <WorkBox img="/images/blog/4-column/8.jpg"
+          <WorkBox
+            img="/images/blog/4-column/8.jpg"
             title="DESIGN YOU NEED EVERYDAY"
             date=" 31 OCTOBER 2015"
           />
